2975faf980827b2a7d339f7655d04b5fefd11b70
Jiri Pokorny <jiri.pokorny@jumpshot.com>
2018-06-02 07:26:42 +0200

    Day 2

--- src/main/scala/introfp.scala
+++ src/main/scala/introfp.scala
@@ -376,7 +376,8 @@ object functions {
 
     //def repeat[A](n: Int, a: A): A = ???
 
-    // type class - bundle of operations on types and laws for operations
+    // type class - bundle of operations, types and laws for operations
+    // enables us to extend not our types
 
     // append: (A, A) => A
     trait SemiGroup[A] {
@@ -405,6 +406,27 @@ object functions {
         override def append(l: String, r: String): String = l + r
       }
 
+      implicit def SemiGroupList[A] = new SemiGroup[List[A]] {
+        override def append(l: List[A], r: List[A]): List[A] = l ++ r
+      }
+
+//      implicit def SemiGroupMap[K, V](implicit A: SemiGroup[V]) = new SemiGroup[Map[K, V]] {
+//        override def append(l: Map[K, V], r: Map[K, V]): Map[K, V] = {
+//          def values(key: K) = List(l.get(key), r.get(key)).flatten
+//          val x = for (key <- l.keySet ++ r.keySet) yield (key, values(key).reduce(A.append))
+//          x.toMap
+//        }
+//      }
+
+      implicit def SemiGroupMap2[K, V: SemiGroup] = new SemiGroup[Map[K, V]] {
+        override def append(l: Map[K, V], r: Map[K, V]): Map[K, V] = {
+          def values(key: K) = List(l.get(key), r.get(key)).flatten
+          val x = for (key <- l.keySet ++ r.keySet) yield (key, values(key).reduce(_ <> _))
+          x.toMap
+        }
+      }
+
+      // Allows us to create a semigroup like this: SemiGroup[Int]()
       def apply[A](implicit S: SemiGroup[A]): SemiGroup[A] = S
     }
 
@@ -438,14 +460,403 @@ object functions {
     }
 
     // context-bound & using apply & using implicit class operator
+    // this method signature is much better in terms of describing what this function does
     def repeat4[A: SemiGroup](n: Int, a: A): A =
       if (n <= 1) a
       else a <> repeat(n - 1, a)
 
     (("Hello", "Hello"), "Goodbye") <> (("Goodbye", "Hello"), "Hello")
 
-    // TODO implement semigroup for list and map
+    Map(1 -> "Bla bla") <> Map(2 -> "Ce ce", 1 -> "Ha ha")
+
+
+
+    trait Monoid[A] extends SemiGroup[A] {
+      // Must satisfy:
+      // Left Zero: append(zero, a) == a
+      // Right Zero: append(a, zero) == a
+      def zero: A
+    }
+
+    object Monoid {
+      def apply[A](implicit M: Monoid[A]): Monoid[A] = M
+
+      implicit val MonoidInt: Monoid[Int] = new Monoid[Int] {
+        override val zero = 0
+
+        override def append(l: Int, r: Int) = l <> r
+      }
+    }
+
+    def zero[A: Monoid]: A = Monoid[A].zero // implicitly instantiate Monoid, thanks to apply() and implicit vals
+
+    zero[Int]
+
+
+    // transitive law: eq(a, b) && eq(b, c) => eq(a, c)
+    // 1 / 10 = 1 / (2 * 5) = (1 / 2) * (1 / 5)
+    // better than equals (that has AnyRef type)
+    trait Eq[A] {
+      def eq(l: A, r: A): Boolean
+    }
+    object Eq {
+      def apply[A](implicit A: Eq[A]): Eq[A] = A
+      implicit val EqInt = new Eq[Int] {
+        def eq(l: Int, r: Int): Boolean = l == r
+      }
+    }
+    implicit class EqSyntax[A](l: A) {
+      def === (r: A)(implicit A: Eq[A]): Boolean = A.eq(l, r)
+    }
+
+    // compiler plugin for typeclasses
+    // https://github.com/mpilquist/simulacrum
+
   }
+
+  object adts {
+    // idea: design types so they do not allow for any kind of error
+
+    case class Email private (value: String)
+    object Email {
+      def apply(value: String): Option[Email] = ???
+
+    }
+
+    // class for representing CORRECT credit card number
+    final case class CreditCardNumber private ()
+    object CreditCardNumber {
+      def apply(number: String): Option[CreditCardNumber] = ???
+    }
+
+    sealed trait PaymentMethod
+    final case class CreditCard(number: String, code: String, name: String) extends PaymentMethod
+    final case object Cash extends PaymentMethod
+    final case class Bank(routingNumber:String, swiftCode: String, accountNumber: String) extends PaymentMethod
+    final case class Bitcoin(sig: String, address: String) extends PaymentMethod
+
+    CreditCard("", "abaabbabab", "432423423423$#@$@#$@#$#@$")
+
+
+    // wrong
+//    class PaymentMethod {
+//      static final int CREDIT_CARD = 1
+//      String getCardNumber();
+//    }
+
+    final case class Employee(
+                               name: String,
+                               start: java.time.Instant,
+                               position: Position
+                             )
+
+    sealed trait Position
+    final case object CEO extends Position
+    final case object CTO extends Position
+    final case class Engineer(level: Int) extends Position
+
+  }
+
+  // https://leanpub.com/fpmortals/read
+
+
+  object functor {
+    // type-class
+    // Identity Law    : fmap(identity)(fa) === fa
+    //     - How to break it? By adding / removing / reshuffling list items
+    //     - We do not change structure by mapping
+    // Composition Law : fmap(f)(fmap(g)(fa)) == fmap(f.compose(g))(fa)
+    //     - Set is not a Functor
+    //
+
+    trait Functor[F[_]] {
+      def fmap[A, B](f: A => B): F[A] => F[B]
+    }
+
+    object Functor {
+      // Functor[List].fmap[Int, String](_.toString)(List(1, 2, 3))
+      def apply[F[_]: Functor]: Functor[F] = implicitly[Functor[F]]
+
+      implicit val OptionFunctor = new Functor[Option] {
+        override def fmap[A, B](f: A => B): Option[A] => Option[B] = _.map(f)
+      }
+      implicit val ListFunctor = new Functor[List] {
+        override def fmap[A, B](f: A => B): List[A] => List[B] = _.map(f)
+      }
+
+      // FunctionFunctor is function composition
+      implicit def FunctionFunctor[A] = new Functor[Function1[A, ?]] {
+        override def fmap[B, C](f: B => C): (A => B) => A => C = x => f.compose(x)
+      }
+    }
+
+    implicit class FunctorSyntax[F[_], A](fa: F[A]) {
+      def fmap[B](f: A => B)(implicit F: Functor[F]): F[B] = F.fmap(f)(fa)
+    }
+
+    List(1, 2, 3).fmap(_.toString)
+
+    // nested mapping!
+    case class Compose[F[_], G[_], A](fga: F[G[A]])
+    // functors compose very well
+    case class Product[F[_], G[_], A](l: F[A], r: G[A])
+    case class Coproduct[F[_], G[_], A](e: Either[F[A], G[A]])
+
+
+    trait Apply[F[_]] extends Functor[F] {
+      def ap[A, B](f: F[A => B], fa: F[A]): F[B]
+      // def zip[A, B](l: F[A], r: F[B]): F[(A, B)]
+    }
+
+    // Example for Apply: Option((i: Int) => String).ap(Option(1))
+
+    object Apply {
+      implicit val OptionApply = new Apply[Option] {
+        override def ap[A, B](f: Option[A => B], fa: Option[A]): Option[B] = (f, fa) match {
+          case (Some(f), Some(a)) => Some(f(a))
+          case _ => None
+        }
+        override def fmap[A, B](f: A => B): Option[A] => Option[B] = _.fmap(f)
+      }
+    }
+
+    trait Applicative[F[_]] extends Apply[F] {
+      def point[A](a: A): F[A] // Example: Some(a), List(a), Future.successful(a), no point for Map (Map is just Apply)
+    }
+
+    object Applicative {
+      implicit val ListApplicative = new Applicative[List] {
+        override def point[A](a: A): List[A] = List(a)
+
+        override def ap[A, B](f: List[A => B], fa: List[A]): List[B] = // lossy implementation: f.zip(fa).map { case (f, x) => f(x) }
+          for {
+            f <- f
+            a <- fa
+          } yield (f(a)) // cartesian product
+
+        override def fmap[A, B](f: A => B): List[A] => List[B] = _.fmap(f)
+      }
+    }
+
+    // Dual types - example point A -> F[A], Coapplicative F[A] -> A
+    // Identity is Monad and Comonad at once
+    final case class Identity[A](run: A)
+
+    trait Monad[F[_]] extends Applicative[F] {
+      // programmable semicolon
+      // intuition with recipes: fa is a recipe for A, F[B] is a recipe for B, A => F[B] is a function for creating a recipe from A
+      //   next recipe depends on value(s) produced by previous recipe
+      // flatMap is very hard to optimize (paralellize), for example independent api calls via Future - Future as instance of Apply
+      def bind[A, B](fa: F[A])(f: A => F[B]): F[B] // flatMap
+    }
+
+    object Monad {
+      implicit val IdentityMonad = new Monad[Identity] {
+        override def bind[A, B](fa: Identity[A])(f: A => Identity[B]): Identity[B] = f(fa.run)
+        override def point[A](a: A): Identity[A] = Identity(a)
+        override def ap[A, B](f: Identity[A => B], fa: Identity[A]): Identity[B] = Identity(f.run(fa.run))
+        override def fmap[A, B](f: A => B): Identity[A] => Identity[B] = x => Identity(f(x.run))
+      }
+    }
+
+    implicit class MonadSyntax[F[_], A](fa: F[A]) {
+      def map[B](f: A => B)(implicit F: Monad[F]): F[B] = F.fmap(f)(fa)
+      def flatMap[B](f: A => F[B])(implicit F: Monad[F]): F[B] = F.bind(fa)(f)
+    }
+
+  }
+
+  object applied {
+    import functor._
+    //def println(s: String): Unit = Predef.println(s) // pure version = ()
+    //def readLine(): String = scala.io.StdIn.readLine() // pure version = ""
+
+    // description of an effectful program that will produce an A
+    final case class IO[A](unsafePerformIO: () => A) {
+      def map[B](f: A => B): IO[B] = IO(() => f(unsafePerformIO()))
+      def flatMap[B](f: A => IO[B]): IO[B] = IO(() => f(unsafePerformIO()).unsafePerformIO()) // NOT f(unsafePerformIO())
+    }
+
+    object IO {
+      def point[A](a: => A): IO[A] = IO(() => a)
+
+      implicit val MonadIO: Monad[IO] = new Monad[IO] {
+        override def bind[A, B](fa: IO[A])(f: A => IO[B]): IO[B] = fa.flatMap(f)
+
+        override def point[A](a: A): IO[A] = point(a)
+
+        override def ap[A, B](f: IO[A => B], fa: IO[A]): IO[B] = ???
+
+        override def fmap[A, B](f: A => B): IO[A] => IO[B] = x => x.map(f)
+      }
+    }
+
+    // deterministic, pure and free of side effects
+//    def println(s: String): IO[Unit] = IO(() => Predef.println(s))
+//    def readLine(): IO[String] = IO(() => scala.io.StdIn.readLine())
+
+    case class Location()
+    case class World(description: String, what: List[Item], options: Map[Location, World])
+
+
+    sealed trait Item
+    case class Food(health: Int, description: String)
+    case class Weapon(damage: Int, description: String)
+
+    case class Player(name: String, inventory: List[Item], location: Location)
+    case class State(player: Player, world: World)
+
+    sealed trait Command
+    case object Exit extends Command
+    case object Look extends Command
+    case class Pickup(what: String) extends Command
+
+    def parseInput(input: String): Either[String, Command] = {
+      val l = input.split(" ").map(_.toLowerCase).toList
+      l match {
+        case "exit" :: Nil => Right(Exit)
+        case "look" :: Nil => Right(Look)
+        case "pickup" :: what :: Nil => Right(Pickup(what))
+        case _ => Left("Invalid command.")
+      }
+    }
+
+    def update(command: Command, state: State): (String, Option[State]) = command match {
+      case Exit => ("Bye!", None)
+      case Look => ("You are standing somwhere, look at something", Some(state))
+    }
+
+    def mainLoop[F[_]: Monad: Console](state: State): F[Unit] = {
+      val maybeContinue: Command => F[Unit] = { (command: Command) =>
+        val (message, next) = update(command, state)
+        for {
+          _ <- Console[F].println(message)
+          _ <- next.fold(implicitly[Monad[F]].point(()))(mainLoop(_))
+        } yield ()
+      }
+
+      def printContinue(line: String): F[Unit] = {
+        for {
+          _ <- Console[F].println(line)
+          _ <- mainLoop(state)
+        } yield ()
+      }
+
+      for {
+        either <- Console[F].readLine.map(parseInput)
+        _ <- either.fold(printContinue, maybeContinue)
+      } yield ()
+    }
+
+
+    def program[F[_]: Monad: Console]: F[Unit] =
+      for {
+        _ <- Console[F].println("What is your name")
+        name <- Console[F].readLine
+        _ <- Console[F].println(s"Hello $name, it's good to meet you!")
+        player = Player(name, Nil, Location())
+        state = State(player, World("Room 204 in Wolf Law Building", Nil, Map()))
+        _ <- mainLoop(state)
+      } yield ()
+
+    trait Console[F[_]] {
+      def println(line: String): F[Unit]
+      def readLine: F[String]
+    }
+
+    object Console {
+      def apply[F[_]](implicit F: Console[F]): Console[F] = F
+      implicit val ConsoleIO: Console[IO] = new Console[IO] {
+        def println(s: String): IO[Unit] = IO(() => Predef.println(s))
+        def readLine(): IO[String] = IO(() => scala.io.StdIn.readLine())
+      }
+    }
+
+    def println[F[_]: Console](line: String): F[Unit] = Console[F].println(line)
+    def readLine[F[_]: Console]: F[String] = Console[F].readLine
+
+    case class Buffer(output: List[String], input: List[String])
+    case class FakeConsole[A](run: Buffer => (Buffer, A))
+    object FakeConsole {
+      implicit val ConsoleFakeConsole = new Console[FakeConsole] {
+        override def println(line: String): FakeConsole[Unit] = FakeConsole(buffer => (buffer.copy(output = line :: buffer.output), ()))
+        override def readLine: FakeConsole[String] = FakeConsole(buffer => buffer.input match {
+          case Nil => throw new Error("Broken test!")
+          case line :: rest => (buffer.copy(input = rest), line)
+        })
+      }
+
+      implicit val MonadFakeConsole = new Monad[FakeConsole] {
+        override def point[A](a: A): FakeConsole[A] = FakeConsole(buffer => (buffer, a))
+        override def ap[A, B](f: FakeConsole[A => B], fa: FakeConsole[A]): FakeConsole[B] = FakeConsole { buffer1 =>
+          val (buffer2, ab) = f.run(buffer1)
+          val (buffer3, a) = fa.run(buffer2)
+          (buffer3, ab(a))
+        }
+        override def fmap[A, B](f: A => B): FakeConsole[A] => FakeConsole[B] =
+          (fa: FakeConsole[A]) => FakeConsole { buffer1 =>
+            val (buffer2, a) = fa.run(buffer1)
+            (buffer2, f(a))
+          }
+        override def bind[A, B](fa: FakeConsole[A])(f: A => FakeConsole[B]): FakeConsole[B] = FakeConsole { buffer1 =>
+          val (buffer2, a) = fa.run(buffer1)
+          f(a).run(buffer2)
+        }
+      }
+    }
+
+    def test: List[String] = program[FakeConsole].run(Buffer(Nil, List("John", "look", "exit")))._1.output
+  }
+
+  // finally tagless
+
+
+  // Optics (Lens)
+  // handling nested structures in FP
+  // Optic[S, T, A, B] - S src set, T target set, A src item, B dest item
+  // Optic[S, A] - S == T, A == B
+
+  // case class Person(name: String)
+  // Lens - focus on a term inside a product - Person.name
+  // Prism - focusing on a term of sum type
+  // Traversal -
+
+  object optics {
+    case class Person(name: String, age: Int)
+
+    object Person {
+      val _Name = Lens[Person, String](_.name, (name: String) => _.copy(name = name))
+      val _Age  = Lens[Person, Int](_.age, (age: Int) => _.copy(age = age))
+    }
+
+    case class Car(driver: Person)
+    object Car {
+      val _Person = Lens[Car, Person](_.driver, person => _.copy(driver = person))
+    }
+
+    case class Lens[S, A](
+                         get: S => A,
+                         set: A => S => S
+                         ) {
+      def >>>[B](that: Lens[A, B]): Lens[S, B] = Lens[S, B](
+        get = (s: S) => (that.get(get(s)) : B),
+        set = (b: B) => (s: S) => set(that.set(b)(get(s)))(s)
+      )
+
+      def modify(f: A => A): S => S =
+        (s: S) => set(f(get(s)))(s)
+    }
+
+    val TestCar = Car(Person("Name", 10))
+
+    import Car._
+    import Person._
+
+    val lens = (_Person >>> _Age): Lens[Car, Int]
+
+    lens.modify(_ + 10)(TestCar)
+  }
+
 
 }
 
